4a80efb9f85431a89c4bd29e564462e999a6cb8d
MYounas <younus.ubit56@gmail.com>
2017-03-19 10:05:25 +0500

    work for get nonroot remaining after first selection

--- Main.py
+++ Main.py
@@ -7,43 +7,69 @@ class Main:
 
         totalSwitches=int(input("How many switches you want?"))
 
-        switches=[]
+        self.switches=[]
 
         for i in range(totalSwitches):
             totalPorts=int(input("Enter total number of ports for switch "+str(i)+":"))
-            switches.append(Switch(i,totalPorts))
+            self.switches.append(Switch(i,totalPorts))
 
         print("Now time for connection configuration")
 
         for i in range(totalSwitches):
             print("Ports configuration for switch ",i)
-            for j in range(switches[i].ports):
+            for j in range(self.switches[i].ports):
                 a = int(input("Enter other switch ID:"))
                 b = int(input("Enter other switch's port ID:"))
-                switches[i].portsConf[j]=["",a,b]
+                self.switches[i].portsConf[j]=["",a,b]
 
-        for i in range(switches[0].ports):
-            switches[0].portsConf[i][0]="dp"
+        for i in range(self.switches[0].ports):
+            self.switches[0].portsConf[i][0]="dp"
 
         #for first time root ports setting
-        oppPorts=self.getOppositePorts("dp",switches[0])
+        oppPorts=self.getOppositePorts("dp",self.switches[0])
 
-        keys=oppPorts.keys()    #keys of oppPorts dict
+        listKeys=list(oppPorts.keys())    #keys of oppPorts dict
 
-        for i in range(len(oppPorts)):
-            for j in range(len(keys)):
+        self.rootSelectionFromRoot(oppPorts,listKeys)
 
-                switchId=keys[j]
-                portNum=oppPorts[switchId]      #port number of switch get from oppPorts switches list
+        #get list of switches jin ke root port set ni hoi basic rp setting k bad
+        nonRootList=self.getNonRootSwitches()
 
-                switches[switchId].portsConf[portNum][0]="rp"      #save as switchs k 2 pr ja k us k ports conf pr ja k dict say key utha k us k port wali value wali jga pr rp likh do
-
+        self.setRPOfNonROOT()
 
         print("thanks")
 
         # for i in range(totalSwitches):
         #     print(vars(switches[i]))
         #     print(dir(switches[i]))
+
+
+    def setRPOfNonROOT(self):
+        pass
+
+    def getNonRootSwitches(self):
+
+        nonRootList=[];rootFlag=False
+
+        for i in range(len(self.switches)):
+            rootFlag = False
+            for j in range(self.switches[i].ports):
+                if self.switches[i].portsConf[j][0]=="rp":
+                    rootFlag=True
+                    break
+                elif self.switches[i].portsConf[j][0]!="rp":
+                    rootFlag = False
+
+            if not rootFlag:
+                nonRootList.append(i)
+
+    def rootSelectionFromRoot(self,oppPorts,listKeys):
+
+        for i in range(len(listKeys)):
+            switchId=listKeys[i]
+            portNum=oppPorts[switchId]      #port number of switch get from oppPorts switches list
+
+            self.switches[switchId].portsConf[portNum][0]="rp"      #save as switchs k 2 pr ja k us k ports conf pr ja k dict say key utha k us k port wali value wali jga pr rp likh do
 
 
     def getOppositePorts(self,portType,switch):
